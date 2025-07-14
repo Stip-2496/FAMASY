0628b8bb5a60dd361789f81efa3b8bec09fdb464
Kevin Stip Pama Campo <pamacampokevinstip@gmail.com>
2025-07-14 16:13:21 -0500

    Configurar módulo de usuarios con relaciones, tablas base y corrección en sessions:
    - Migración de las tablas: rol, contacto y direccion
    
    - Creación completa de la tabla users
    
    -Integración de claves foráneas hacia rol y contacto
    
    - Corrección de relación en la tabla sessions con users.user_id

---
+++ database/migrations/2025_07_14_195029_create_direccion_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('direccion', function (Blueprint $table) {
+            $table->id('idDir');
+            $table->unsignedBigInteger('idConDir');
+
+            $table->string('calDir', 20);
+            $table->string('barDir', 20);
+            $table->string('ciuDir', 20);
+            $table->string('depDir', 20);
+            $table->string('codPosDir', 20);
+            $table->string('paiDir', 20);
+
+            $table->foreign('idConDir')->references('idCon')->on('contacto')
+              ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('direccion');
+    }
+};
